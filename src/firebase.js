4a8bf5f115b1a468f0eef8ae0d21f753cd3c7e09
Dipak Patil <dpatil450331@gmail.com>
2026-05-26 17:57:22 +0530

    files added

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "your_api_key",
+  authDomain: "your_project.firebaseapp.com",
+  projectId: "your_project_id",
+  storageBucket: "your_project.appspot.com",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id"
+};
+
+// Init Firebase
+const app = initializeApp(firebaseConfig);
+
+// Auth
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export const loginWithGoogle = () => signInWithPopup(auth, provider);
+export const logout = () => signOut(auth);
+
+// Firestore
+export const db = getFirestore(app);
